736b0e069ae55d59685c953a4945bd8731283da9
Douglas Soares <douglas.srs12@gmail.com>
2018-02-09 17:05:27 -0200

    Add ActiveRecord as Storage

---
+++ Model/PaymentSecurityToken.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * User: martyn ling <mling@orthomeo.com>
+ * Date: 04/12/13
+ * Time: 18:22
+ */
+
+namespace Payum\YiiExtension\Model;
+
+use Payum\Core\Model\Token;
+
+class PaymentSecurityToken extends Token
+{
+    /**
+     * @var \CActiveRecord
+     */
+    public $activeRecord;
+
+    public function __construct($scenario = 'insert', $tableName = '')
+    {
+        if ($scenario == 'insert') {
+            $this->activeRecord = new TokenActiveRecord('insert', $tableName);
+            $this->hash = $this->activeRecord->_hash;
+        }
+    }
+
+    public function primaryKey()
+    {
+        return $this->activeRecord->primaryKey();
+    }
+
+    public function save()
+    {
+        $this->activeRecord->save();
+    }
+
+    public function delete()
+    {
+        $this->activeRecord->delete();
+    }
+
+    public static function findModelById($tableName, $id)
+    {
+        $token = new PaymentSecurityToken('update');
+        $token->activeRecord = TokenActiveRecord::model($tableName)->findByPk($id);
+
+        // Load the values into the token object from the activeRecord
+        $token->hash = $token->activeRecord->_hash;
+        $token->targetUrl = $token->activeRecord->_target_url;
+        $token->afterUrl = $token->activeRecord->_after_url;
+        $token->paymentName = $token->activeRecord->_payment_name;
+        $token->details = $token->activeRecord->getDetailsIdentificator();
+        return $token;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return Identificator
+     */
+    public function getDetails()
+    {
+        return $this->details;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setDetails($details)
+    {
+         $this->activeRecord->_details = $this->details = $details;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getHash()
+    {
+        return $this->hash;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setHash($hash)
+    {
+        $this->hash = $this->activeRecord->_hash = $hash;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTargetUrl()
+    {
+        return $this->targetUrl;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setTargetUrl($targetUrl)
+    {
+        $this->targetUrl = $this->activeRecord->_target_url = $targetUrl;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAfterUrl()
+    {
+        return $this->afterUrl;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setAfterUrl($afterUrl)
+    {
+        $this->afterUrl = $this->activeRecord->_after_url = $afterUrl;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getPaymentName()
+    {
+        return $this->paymentName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setPaymentName($paymentName)
+    {
+        $this->paymentName = $this->activeRecord->_payment_name = $paymentName;
+    }
+}
---
+++ Storage/ActiveRecordStorage.php
@@ -0,0 +1,84 @@
+<?php
+namespace Payum\YiiExtension\Storage;
+
+use InvalidArgumentException;
+use Payum\Core\Exception\LogicException;
+use Payum\Core\Model\ArrayObject;
+use Payum\Core\Model\Identificator;
+use Payum\Core\Storage\AbstractStorage;
+
+class ActiveRecordStorage extends AbstractStorage
+{
+    protected $_tableName;
+
+    public function __construct($tableName, $modelClass)
+    {
+        parent::__construct($modelClass);
+
+        $this->_tableName = $tableName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createModel()
+    {
+        return new $this->modelClass('insert', $this->_tableName);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function doUpdateModel($model)
+    {
+        $model->save();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function doDeleteModel($model)
+    {
+        $model->delete();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function doGetIdentificator($model)
+    {
+        if (is_array($model->primaryKey())) {
+            throw new LogicException('Composite primary keys are not supported by this storage.');
+        }
+
+        if ($model instanceof ArrayObject) {
+            return new Identificator($model[$model->primaryKey()], $this->modelClass);
+        } else {
+            return new Identificator($model->{$model->primaryKey()}, $this->modelClass);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    function findModelById($id)
+    {
+        $className = $this->modelClass;
+        return $className::findModelById($this->_tableName, $id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function assertModelSupported($model)
+    {
+        parent::assertModelSupported($model);
+
+        if (!property_exists(get_class($model), 'activeRecord')
+            || false == $model->activeRecord instanceof \CActiveRecord) {
+            throw new InvalidArgumentException(
+                'Model required to have activeRecord property, which should be sub class of CActiveRecord class.'
+            );
+        }
+    }
+}
